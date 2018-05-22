ae877629ccce9a0a0282f339d8e8250a108afcc2
Blair Jersyer <Blair2004@users.noreply.github.com>
2018-05-22 10:11:28 +0100

    Update - module view

--- src/resources/views/components/backend/modules.blade.php
+++ src/resources/views/components/backend/modules.blade.php
@@ -12,9 +12,6 @@ $tabs           =   [
     ], [
         'namespace' =>  'install',
         'title'     =>  __( 'Install' )
-    ], [
-        'namespace' =>  'store',
-        'title'     =>  __( 'Store' )
     ]
 ]
 @endphp
@@ -58,34 +55,63 @@ $tabs           =   [
                             v-for="tab in tabs"
                             :key="tab.namespace"
                         >
-                            <v-layout row>
-                                <v-flex xs12 sm6 md4 lg3 xl3 v-for="module of modules">
-                                    <v-card>
-                                        <v-card-title primary-title>
-                                            <div>
-                                                <h3 class="headline mb-0">@{{ module.name }}</h3>
-                                                <div>@{{ module.description }} <small>( @{{ module.version }} )</small></div>
-                                            </div>
-                                        </v-card-title>
-                                        <v-card-actions>
-                                            <v-btn-toggle v-model="module.toggles">
-                                                <v-btn v-if="module.enabled" :href="'{{ route( 'dashboard.modules.enable', [ 'namespace' => '' ] ) }}/' + module.namespace" flat color="orange">
-                                                    <v-icon>check_circle</v-icon>
-                                                </v-btn>
-                                                <v-btn v-else-if="! module.enabled" :href="'{{ route( 'dashboard.modules.disable', [ 'namespace' => '' ] ) }}'/ + module.namespace" flat color="primary">
-                                                    <v-icon>highlight_off</v-icon>
-                                                </v-btn>
-                                                <v-btn :href="'{{ route( 'dashboard.modules.delete', [ 'namespace' => '' ] ) }}/' + module.namespace" flat color="red">
-                                                    <v-icon>delete_forever</v-icon>
-                                                </v-btn>
-                                                <v-btn :href="'{{ route( 'dashboard.modules.extract', [ 'namespace' => '' ] ) }}/' + module.namespace" flat color="green">
-                                                    <v-icon>file_download</v-icon>
-                                                </v-btn>
-                                            </v-btn-toggle>
-                                        </v-card-actions>
-                                    </v-card>
-                                </v-flex>
-                            </v-layout>
+                            <template v-for="modules of modulesRows">
+                                <v-layout row>
+                                    <template>
+                                        <v-flex xs12 sm6 md4 lg3 xl3 v-for="module of modules">
+                                            <v-card>
+                                                <v-container fluid grid-list-lg>
+                                                    <v-layout row>
+                                                    <v-flex xs7>
+                                                        <div>
+                                                            <div class="headline">Supermodel</div>
+                                                            <div>Foster the People</div>
+                                                        </div>
+                                                    </v-flex>
+                                                    <v-flex xs5>
+                                                        <v-card-media
+                                                        src="/static/doc-images/cards/foster.jpg"
+                                                        height="125px"
+                                                        contain
+                                                        ></v-card-media>
+                                                    </v-flex>
+                                                    </v-layout>
+                                                </v-container>
+                                                <v-divider></v-divider>
+                                                <v-card-actions>
+                                                    <v-btn flat color="orange">Share</v-btn>
+                                                    <v-btn flat color="orange">Explore</v-btn>
+                                                </v-card-actions>
+                                            </v-card>
+                                        </v-flex>
+                                    </template>
+                                        <!-- <v-card>
+                                            <v-card-title primary-title>
+                                                <div>
+                                                    <h3 class="headline mb-0">@{{ module.name }}</h3>
+                                                    <div>@{{ module.description }} <small>( @{{ module.version }} )</small></div>
+                                                </div>
+                                            </v-card-title>
+                                            <v-card-actions>
+                                                <v-btn-toggle v-model="module.toggles">
+                                                    <v-btn v-if="module.enabled" :href="'{{ route( 'dashboard.modules.enable', [ 'namespace' => '' ] ) }}/' + module.namespace" flat color="orange">
+                                                        <v-icon>check_circle</v-icon>
+                                                    </v-btn>
+                                                    <v-btn v-else-if="! module.enabled" :href="'{{ route( 'dashboard.modules.disable', [ 'namespace' => '' ] ) }}'/ + module.namespace" flat color="primary">
+                                                        <v-icon>highlight_off</v-icon>
+                                                    </v-btn>
+                                                    <v-btn :href="'{{ route( 'dashboard.modules.delete', [ 'namespace' => '' ] ) }}/' + module.namespace" flat color="red">
+                                                        <v-icon>delete_forever</v-icon>
+                                                    </v-btn>
+                                                    <v-btn :href="'{{ route( 'dashboard.modules.extract', [ 'namespace' => '' ] ) }}/' + module.namespace" flat color="green">
+                                                        <v-icon>file_download</v-icon>
+                                                    </v-btn>
+                                                </v-btn-toggle>
+                                            </v-card-actions>
+                                        </v-card> -->
+                                </v-layout>
+                                <v-divider></v-divider>
+                            </template>
                         </v-tab-item>
                     </v-tabs>
                 </div>
@@ -98,7 +124,8 @@ $tabs           =   [
 <script>
 var data    =   {
     modules     :   @json( $collection ),
-    tabs        :   @json( $tabs )
+    tabs        :   @json( $tabs ),
+    perLines    :   1
 }
 Vue.component( 'app-modules', {
     data() {
@@ -107,6 +134,16 @@ Vue.component( 'app-modules', {
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         });
     },
+    computed : {
+        modulesRows() {
+            let totalLines  =   Math.round( Object.values( this.modules ).length / this.perLines );
+            let modulesRows     =   [];
+            for ( let i = 0; i < totalLines ; i++ ) {
+                modulesRows[i]  =   Object.values( this.modules ).splice( i * this.perLines, this.perLines );
+            }
+            return modulesRows;
+        }
+    }
 })
 </script>
 <!-- <script src="{{ asset( 'tendoo/js/components/modules.js' ) }}"></script> -->
