9234526e71f219c8fd777ff3cb46148f7ba5ab74
blair2004 <carlosjohnsonluv2004@gmail.com>
2018-10-16 09:04:39 +0100

    Update
    - Adding API Controllers

---
+++ src/core/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\EnvatoChecker\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
+use Tendoo\Core\Http\Controllers\ApiController;
+use Carbon\Carbon;
+
+
+class ApiAppController extends Controller
+{
+
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,18 +1,25 @@
 <?php
-
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::middleware([ 'api.guard' ])
+    ->prefix( 'api' )
+    ->group( function() {
+        Route::get( 'status', 'ApiAppController@status' );
+        Route::post( 'setup/database', 'ApiAppController@setupDatabase' );
+        Route::post( 'setup/details', 'ApiAppController@setupDatabase' );
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+        Route::get( 'modules', 'ApiModulesController@modules' );
+        Route::get( 'module/{namespace}', 'ApiModulesController@module' );
+        Route::post( 'module', 'ApiModulesController@upload' );
+        Route::post( 'module/{namespace}/{action}', 'ApiModulesController@changeState' );
+        Route::module( 'module/{namespace}', 'ApiModulesController@remove' );
+        Route::delete( 'module/{namespace}', 'ApiModulesController@uninstall' );
+
+        Route::get( 'users', 'ApiUsersController@users' );
+
+        Route::get( 'user/{id}', 'ApiUsersController@user' );
+        Route::post( 'user', 'ApiUsersController@create' );
+        Route::put( 'user', 'ApiusersController@update' );
+        Route::delete( 'user/{id}', 'ApiUsersController@delete' );
 });
+
